792274b5d9def63b708dcf7f94c9e97914c198b1
Kanishk Gandhi <kanishk050296@gmail.com>
2021-03-24 16:34:48 -0400

    debug models.py

--- job_tom.sh
+++ job_tom.sh
@@ -12,4 +12,4 @@
 cd /data/kvg245/bib-tom
 source venv/bin/activate
 git pull
-python3 train_tom.py --data_path /misc/vlgscratch4/LakeGroup/kanishk/bg_train/aug350k --types co pr --batch_size 512 --max_epochs 100 --val_check_interval 1 --gpus 1 --gradient_clip_val 10 --num_workers 16 --track_grad_norm 2 --num_sanity_val_steps 2 --stochastic_weight_avg True
+python3 train_tom.py --data_path /misc/vlgscratch4/LakeGroup/kanishk/bg_train/aug280k --types co pr --batch_size 512 --max_epochs 100 --val_check_interval 1 --gpus 1 --gradient_clip_val 10 --num_workers 16 --track_grad_norm 2 --num_sanity_val_steps 2 --stochastic_weight_avg True
