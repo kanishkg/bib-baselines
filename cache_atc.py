7b5923ea5f5256f583f3020a7247eaa0f841a2ff
Kanishk Gandhi <kanishk050296@gmail.com>
2021-04-02 20:19:50 -0400

    to cuda cache py

--- cache_atc.py
+++ cache_atc.py
@@ -44,7 +44,7 @@ torch.manual_seed(args.seed)
 
 # load model with weights and hparams in eval mode
 model = ATCEncoder.load_from_checkpoint(args.ckpt, hparams_file=args.hparams)
-model.cuda()
+# model.cuda()
 model.eval()
 model.freeze()
 
@@ -74,7 +74,7 @@ if not args.test:
     for b, batch in enumerate(train_loader):
         print(f'train {b}/ {len(train_dataset)}')
         frames, actions = batch
-        frame_embeddings = model(frames.cuda())
+        frame_embeddings = model(frames)
         frame_embeddings = frame_embeddings[0, :, :].cpu().numpy()
         actions = actions[0, :].cpu().numpy()
         store_dict[f'{b}_s'] = frame_embeddings
@@ -90,7 +90,7 @@ if not args.test:
         for b, batch in enumerate(v):
             print(f'val {b}')
             frames, actions = batch
-            frame_embeddings = model(frames.cuda())
+            frame_embeddings = model(frames)
             frame_embeddings = frame_embeddings[0, :, :].cpu().numpy()
             actions = actions[0, :].cpu().numpy()
             store_dict[f'{b}_s'] = frame_embeddings
@@ -117,7 +117,7 @@ else:
         for b, batch in enumerate(v):
             print(f'test {b}')
             frames, actions = batch
-            frame_embeddings = model(frames.cuda())
+            frame_embeddings = model(frames)
             frame_embeddings = frame_embeddings[0, :, :].cpu().numpy()
             actions = actions[0, :].cpu().numpy()
             store_dict[f'{b}_s'] = frame_embeddings
