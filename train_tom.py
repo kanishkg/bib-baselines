92510124abe53a8dbc0a41ea01815234a183c6df
Kanishk Gandhi <kanishk050296@gmail.com>
2021-05-10 19:19:46 -0400

    add GAIL

--- train_tom.py
+++ train_tom.py
@@ -21,7 +21,7 @@ parser.add_argument('--num_workers', type=int, default=4)
 parser.add_argument('--batch_size', type=int, default=256)
 
 # add model specific args
-parser = ContextAIL.add_model_specific_args(parser)
+parser = ContextImitationPixel.add_model_specific_args(parser)
 
 # add all the available trainer options to argparse
 parser = Trainer.add_argparse_args(parser)
@@ -36,7 +36,7 @@ np.random.seed(args.seed)
 torch.manual_seed(args.seed)
 
 # init model
-model = ContextAIL(args)
+model = ContextImitationPixel(args)
 
 # most basic trainer, uses good defaults (auto-tensorboard, checkpoints, logs, and more)
 trainer = Trainer.from_argparse_args(args)
