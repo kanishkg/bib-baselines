8e87bb1d91fc5c645c8f9e6d5a4cad146a5a90de
Kanishk Gandhi <kvg245@cassio.cs.nyu.edu>
2021-12-15 16:28:23 -0500

    add test file

--- job_tom.sh
+++ job_tom.sh
@@ -3,8 +3,8 @@
 #SBATCH --mail-type=ALL         # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=kvg245@nyu.edu    # Where to send mail
 #SBATCH --nodes=1                    # Run all processes on  a single node
-#SBATCH --ntasks=16                  # Number of proces ses
-#SBATCH --gpus=1
+#SBATCH --ntasks=12                  # Number of proces ses
+#SBATCH --gres=gpu:1080ti:1
 #SBATCH --mem=100GB                   # Total memory limit
 #SBATCH --time=48:00:00              # Time limit hrs:min:sec
 #SBATCH --output=%j.log # Standard output and error log
@@ -12,4 +12,5 @@
 cd /data/kvg245/bib-tom
 source venv/bin/activate
 git pull
-python3 train_tom.py --data_path /misc/vlgscratch4/LakeGroup/kanishk/bg_train/aug280k --types co pr --batch_size 512 --max_epochs 100 --val_check_interval 1 --gpus 1 --gradient_clip_val 10 --num_workers 16 --track_grad_norm 2 --num_sanity_val_steps 2 --stochastic_weight_avg True
+# python3 train_tom.py --data_path /misc/vlgscratch4/LakeGroup/kanishk/bg_train/ --types coe pre mue see --seed 1 --batch_size 32 --max_epochs 1000 --gpus 1 --num_workers 12  --stochastic_weight_avg True --lr 1e-4 --check_val_every_n_epoch 1 --max_epochs 1000 --process_data 0 --track_grad_norm 2 --gradient_clip_val 0.5 
+python3 train_tom.py --data_path /misc/vlgscratch4/LakeGroup/kanishk/bg_train/ --types coe pre mue see --seed 1 --batch_size 64 --max_epochs 1000 --gpus 1 --num_workers 12  --stochastic_weight_avg False --lr 1e-5 --check_val_every_n_epoch 1 --max_epochs 1000 --process_data 0 --track_grad_norm 2 --gradient_clip_val 0.5 --resume_from_checkpoint /data/kvg245/bib-tom/lightning_logs/version_952410/checkpoints/epoch\=63-step\=22399.ckpt
--- test_seq.py
+++ test_seq.py
@@ -2,39 +2,56 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 import torch
+
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
 import pytorch_lightning as pl
+
 from irl.datasets import TestSeqTransitionDataset
 from irl.models import ContextImitationLSTM
 from irl.datasets import TestRawTransitionDataset
 from irl.models import ContextImitationPixel
 
-model = ContextImitationLSTM.load_from_checkpoint('lightning_logs/version_947097/checkpoints/epoch=17-step=12599.ckpt')
-# model = ContextImitationPixel.load_from_checkpoint('lightning_logs/version_932742/checkpoints/epoch=26-step=9449.ckpt')
+mode = 'rnn'
+if mode=='mlp':
+    model = ContextImitationPixel.load_from_checkpoint('lightning_logs/version_932742/checkpoints/epoch=26-step=9449.ckpt')
+    test_datasets = TestRawTransitionDataset('/misc/vlgscratch4/LakeGroup/kanishk/bib-dataset/bib_eval_2/', types='ee', size=(84,84), process_data=1, mode='test')
+elif mode=='rnn':
+    model = ContextImitationLSTM.load_from_checkpoint('lightning_logs/version_947097/checkpoints/epoch=17-step=12599.ckpt')
+    test_datasets = TestSeqTransitionDataset('/misc/vlgscratch4/LakeGroup/kanishk/bib-dataset/bib_eval_2/', types='ee', size=(84,84), process_data=0, mode='test')
 model.eval()
 
-# test_datasets = TestRawTransitionDataset('/misc/vlgscratch4/LakeGroup/kanishk/bib-dataset/bib_eval_2/', types='gs', size=(84,84), process_data=0, mode='test')
-test_datasets = TestSeqTransitionDataset('/misc/vlgscratch4/LakeGroup/kanishk/bib-dataset/bib_eval_2/', types='fs', size=(84,84), process_data=0, mode='test')
 
-from torch.utils.data import DataLoader
 test_dataloader = DataLoader(test_datasets, batch_size=1, num_workers=1, pin_memory=True, shuffle=False)
 
 
-import torch.nn.functional as F
-from tqdm import tqdm
 c = 0
 pbar = tqdm(test_dataloader)
 
 for j, batch in enumerate(pbar):
-    dem_expected_states, dem_expected_actions, dem_expected_lens, query_expected_frames, target_expected_actions, \
-        dem_unexpected_states, dem_unexpected_actions, dem_unexpected_lens, query_unexpected_frames, target_unexpected_actions = batch
+    if mode == 'mlp':
+        dem_expected_states, dem_expected_actions, query_expected_frames, target_expected_actions, \
+            dem_unexpected_states, dem_unexpected_actions, query_unexpected_frames, target_unexpected_actions = batch
+
+    elif mode == 'rnn':
+        dem_expected_states, dem_expected_actions, dem_expected_lens, query_expected_frames, target_expected_actions, \
+            dem_unexpected_states, dem_unexpected_actions, dem_unexpected_lens, query_unexpected_frames, target_unexpected_actions = batch
+
     surprise_expected = []
     em = -1
     max_surp = 0
     te, teh = None, None
 
     for i in range(query_expected_frames.size(1)):
-        test_actions, test_actions_pred = model(
-            [dem_expected_states, dem_expected_actions, dem_expected_lens, query_expected_frames[:, i, :, :, :], target_expected_actions[:,i,:]])
+        if mode == 'mlp':
+            test_actions, test_actions_pred = model(
+                    [dem_expected_states, dem_expected_actions, query_expected_frames[:, i:i+1, :, :, :], target_expected_actions[:,i,:]])
+
+
+        elif mode == 'rnn':
+            test_actions, test_actions_pred = model(
+                [dem_expected_states, dem_expected_actions, dem_expected_lens, query_expected_frames[:, i, :, :, :], target_expected_actions[:,i,:]])
 
         loss = F.mse_loss(test_actions, test_actions_pred)
         if loss.cpu().detach().numpy() > max_surp:
@@ -54,8 +71,13 @@ for j, batch in enumerate(pbar):
     tu, tuh = None, None
 
     for i in range(query_unexpected_frames.size(1)):
-        test_actions, test_actions_pred = model(
-            [dem_unexpected_states, dem_unexpected_actions, dem_unexpected_lens, query_unexpected_frames[:, i, :, :, :], target_unexpected_actions[:,i,:]])
+        if mode == 'mlp':
+            test_actions, test_actions_pred = model(
+                    [dem_unexpected_states, dem_unexpected_actions, query_unexpected_frames[:, i:i+1, :, :, :], target_unexpected_actions[:,i,:]])
+
+        elif mode == 'rnn':
+            test_actions, test_actions_pred = model(
+                [dem_unexpected_states, dem_unexpected_actions, dem_unexpected_lens, query_unexpected_frames[:, i, :, :, :], target_unexpected_actions[:,i,:]])
 
 
         loss = F.mse_loss(test_actions, test_actions_pred)
